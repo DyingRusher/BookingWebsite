bd86af36fa4315e5b50406a8c0fbcb92537195d4
DyingRusher <arjavankoliya@gmail.com>
2023-09-07 15:08:38 +0530

    Added header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        search:'#F5305A'
+      }
+    },
   },
   plugins: [],
 }
